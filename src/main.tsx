f87bf479a5927eddca0556e9c576d6d69f853740
Jonathan Pe <jonathanqpe@gmail.com>
2025-03-13 18:45:15 -0700

    install shadcn/tailwindcss and add Poppins font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import '@fontsource/poppins/index.css'
 
 import './index.css'
 import App from './App.tsx'
