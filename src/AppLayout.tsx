c179e91f111b6e640e5bbf978181294994b5b7ea
Jonathan Pe <jonathanqpe@gmail.com>
2025-03-13 17:28:24 -0700

    add react-router

---
+++ src/AppLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router'
+
+const AppLayout = () => {
+  return (
+    <div>
+      <h1>App Layout</h1>
+      <Outlet />
+    </div>
+  )
+}
+
+export default AppLayout
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import './index.css'
 import App from './App.tsx'
+import AppLayout from './AppLayout.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<App />} />
+
+          <Route path='about' element={<h1>About</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
