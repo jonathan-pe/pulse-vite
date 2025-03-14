efa2c4f04a2ee021a021a3a5a1c1b4366a21d88c
Jonathan Pe <jonathanqpe@gmail.com>
2025-03-13 20:57:37 -0700

    declutter 404 page

--- src/NotFoundPage.tsx
+++ src/NotFoundPage.tsx
@@ -4,9 +4,8 @@ import { Link } from 'react-router'
 const NotFoundPage: React.FC = () => {
   return (
     <div className='mx-auto flex min-h-screen w-full flex-col p-4 items-center justify-center'>
-      <h1 className='text-6xl font-bold'>404</h1>
-      <h2 className='text-4xl text-muted-foreground'>Page Not Found</h2>
-      <p className='my-8'>Sorry, the page you are looking for does not exist.</p>
+      <h1 className='text-4xl font-bold'>Page Not Found</h1>
+      <p className='mb-8 text-lg'>Sorry, the page you are looking for does not exist.</p>
       <Link to='/' className='text-primary hover:opacity-80'>
         Go back to Home
       </Link>
